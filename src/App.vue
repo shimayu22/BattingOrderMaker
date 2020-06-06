2767b42646c136b12a964083b5e97926cc67e2bd
Yutaka Shimada <shimayu22@gmail.com>
2020-06-07 01:55:33 +0900

    ラジオボタンを修正

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div id="app">
       <h1>ぼくのかんがえたさいきょーのおーだー2020</h1>
       <h2>リーグを選んでね</h2>
-      <input type="radio" name="select_league" id="central" value="セ・リーグ" v-model="league" checked>
+      <input type="radio" id="central" value="セ・リーグ" v-model="league" checked>
       <label for="central">セ・リーグ</label>
-      <input type="radio" name="select_league" id="pacific" value="パ・リーグ" v-model="league">
+      <input type="radio" id="pacific" value="パ・リーグ" v-model="league">
       <label for="pacific">パ・リーグ</label><br>
       <span>選択したのは{{ league }}です。</span>
       <h2>球団を選んでね</h2>
@@ -17,7 +17,8 @@ export default {
   name: 'app',
   data () {
     return {
-      league: ''
+      league: '',
+      team: ''
     }
   }
 }
