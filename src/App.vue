6dbaa7440903e60cdb0061583c64770c99aba9c2
Yutaka Shimada <shimayu22@gmail.com>
2020-06-08 00:33:46 +0900

    リーグによって表示するポジションを変える

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,13 @@
       <h2>打順を組んでね</h2>
       <ol>
         <li v-for="(n, index) in 9" :key="index">
-          <select v-model="selectPosition">
+          <select v-model="selectPosition" v-if="selectLeague === 'central'">
             <option disabled value="">ポジション</option>
-            <option v-for="(position, index) in positions" :key="index">{{ position }}</option>
+            <option v-for="(position, index) in centralLeaguePositions" :key="index">{{ position }}</option>
+          </select>
+          <select v-model="selectPosition" v-if="selectLeague === 'pacific'">
+            <option disabled value="">ポジション</option>
+            <option v-for="(position, index) in pacificLeaguePositions" :key="index">{{ position }}</option>
           </select>
           <select v-model="selectPlayer">
             <option disabled value="">選手</option>
@@ -40,7 +44,8 @@ export default {
       centralLeague: ['読売ジャイアンツ', '横浜DeNAベイスターズ', '阪神タイガース', '広島東洋カープ', '中日ドラゴンズ', '東京ヤクルトスワローズ'],
       pacificLeague: ['埼玉西武ライオンズ', '福岡ソフトバンクホークス', '東北楽天ゴールデンイーグルス', '千葉ロッテマリーンズ', '北海道日本ハムファイターズ', 'オリックス・バファローズ'],
       selectTeam: '',
-      positions: ['投', '捕', '一', '二', '三', '遊', '左', '中', '右', 'DH'],
+      centralLeaguePositions: ['投', '捕', '一', '二', '三', '遊', '左', '中', '右'],
+      pacificLeaguePositions: ['捕', '一', '二', '三', '遊', '左', '中', '右', 'DH'],
       players: ['清水優心', '中田翔', '渡邉諒', '平沼翔太', '中島卓也', '王柏融', '西川遥輝', '大田泰示', '近藤健介']
     }
   }
