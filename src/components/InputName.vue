fb768cdec30161154ad1d4bce0198165d7bd0044
Yutaka Shimada <shimayu22@gmail.com>
2020-06-26 10:36:16 +0900

    リセットボタンを押した時にinputにテキストが残るバグを修正

--- src/components/InputName.vue
+++ src/components/InputName.vue
@@ -3,9 +3,20 @@
     <div>
       <h2 class="pb-2">監督名を入れてね（任意）</h2>
       <div class="input-group">
-        <input class="form-control" placeholder="ななしさん" @input="$emit('input', $event.target.value)">
+        <input class="form-control" placeholder="ななしさん" :value="value" @input="$emit('input', $event.target.value)">
         <div class="input-group-append"><span class="input-group-text">監督</span></div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'InputName',
+  props: {
+    value: {
+      type: String
+    }
+  }
+}
+</script>
